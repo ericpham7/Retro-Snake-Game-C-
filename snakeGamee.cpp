33c5bbcc2fcedefca8266503d191b300bd80de42
EricSyntax <ericpham0902@gmail.com>
2025-12-27 02:08:48 -0800

    Refactor and modularize game logic, add Food class
    Extracted Food logic into separate Food.cpp and Food.h files, and moved global variables to globalVariables.h for better modularity. Updated CMakeLists.txt to include new source files. Refactored snake.cpp and snake.h to use global variables header. Enhanced snakeGamee.cpp to use the new Food class, improved audio handling, and cleaned up code structure.

--- snakeGamee.cpp
+++ snakeGamee.cpp
@@ -3,97 +3,48 @@ using namespace std;
 #include "raylib.h"
 #include <raymath.h>
 #include <deque>
-using namespace std;
 #include "snake.h"
+#include "Food.h"
 
-extern Color green = {173, 204, 96, 255};
-extern Color darkGreen = {43, 51, 24, 255};
-
+// global variables (all declared in globalVariables.h for cross-file access)
+Color green = {173, 204, 96, 255};
+Color darkGreen = {43, 51, 24, 255};
 int cellSize = 30; // 30 pixels
-int cellCount = 25; // 25 cells on each row of grid
+int cellCount = 25; // 25 cells on each row of grid  
 int offset = 75; // width of window borders
-double lastUpdateTime = 0;
-const double moveInterval = 0.2;   // snake moves every 0.2s (used for smooth interpolation)
+double lastUpdateTime = 0; // last time the snake moved (0 by default)
+const double moveInterval = 0.2; // snake moves every 0.2s (used for smooth interpolation)
 
-bool elementInDeque(Vector2 element, deque<Vector2> deque) {
-    for (unsigned int i = 0; i < deque.size(); i++) {
-        if (deque[i] == element) {
-            return true;
-        }
-    }
-    return false;
-};
+bool elementInDeque(Vector2 element, deque<Vector2> deque); // prevent food spawning in snake body positions(deque elements)
+bool eventTriggered(double interval); // interval update checker
 
-bool eventTriggered(double interval) {
-    double currentTime = GetTime();
-    if (currentTime - lastUpdateTime >= interval) {
-        lastUpdateTime = currentTime;
-        return true;
-    }
-    return false;
-};
 
-// Food Objects
-class Food {
-public:
-    // {x,y}
-    Vector2 position;
-    Texture2D texture;
-    
-    // Food Object Constructor
-    Food(deque<Vector2> snakeBody) {
-        Image image = LoadImage("greenapple.png");
-        ImageResize(&image, cellSize, cellSize);  // Resize food image to 30x30
-        texture = LoadTextureFromImage(image); //
-        UnloadImage(image); // unload image to free memory
-        position = GenerateRandomPosition(snakeBody);
-    }
-    // Texture destructor
-    ~Food() {
-        UnloadTexture(texture);
-    }
-
-    void Draw() {
-        DrawTexture(texture, offset + position.x * cellSize, offset + position.y * cellSize, WHITE);
-    }
-
-    Vector2 GenerateRandomCell() {
-        float x = GetRandomValue(0, cellCount -1);
-        float y = GetRandomValue(0, cellCount -1);
-        return Vector2{x, y};
-    }
-    
-    // Position Randomizer
-    Vector2 GenerateRandomPosition(deque<Vector2> snakeBody) {
-        Vector2 position = GenerateRandomCell();
-
-        while(elementInDeque(position, snakeBody)) {
-        position = GenerateRandomCell();
-        }
-        
-        return position;
-    }
- 
-};
-
+// Main Game class to manage game state
 class Game {
 public:
     Snake snake;
     Food food = Food(snake.body);
-    bool running = true;
     int score = 0;
     Music backgroundSound;
     Sound eatSound;
+    Sound borderCollisionSound;
+    bool running;
 
     Game() {
+        running = true;
         InitAudioDevice();
         backgroundSound = LoadMusicStream("Mo'Jitos Mo'Problems.mp3");
         eatSound = LoadSound("mario.mp3");
+        borderCollisionSound = LoadSound("erro.mp3");
+        SetMusicVolume(backgroundSound, 0.5f);
+        SetMusicPitch(backgroundSound, 1.25f);
         PlayMusicStream(backgroundSound);
     }
 
     ~Game() {
         UnloadMusicStream(backgroundSound);
+        UnloadSound(eatSound);
+        UnloadSound(borderCollisionSound);
         CloseAudioDevice();
     }
     
@@ -101,7 +52,7 @@ public:
         food.Draw();
         snake.Draw();
     }
-
+    
     void Update() {
         if (running) {
             snake.Update();
@@ -122,16 +73,18 @@ public:
 
     void checkCollisionWithEdges() {
         if (snake.body[0].x == cellCount || snake.body[0].x == -1) {
+            PlaySound(borderCollisionSound);
             GameOver();
         }
         if (snake.body[0].y == cellCount || snake.body[0].y == -1) {
+            PlaySound(borderCollisionSound);
             GameOver();
         }
     }
 
     void GameOver() {
         snake.reset();
-        food.GenerateRandomPosition(snake.body);
+        food.position = food.GenerateRandomPosition(snake.body);
         running = false;
         score = 0;
         StopMusicStream(backgroundSound);
@@ -139,8 +92,10 @@ public:
 
     void checkCollisionWithTail() {
         deque<Vector2> headlessBody = snake.body;
-        headlessBody.pop_front();
+        headlessBody.pop_front(); // remove head element ([0])from body deque
+        
         if (elementInDeque(snake.body[0], headlessBody)) {
+            PlaySound(borderCollisionSound);
             GameOver();
         }
     }
@@ -148,19 +103,19 @@ public:
 
 
 int main () {
-
-    cout << "Starting the game window...\n";
-    
+   
     // InitWindow(width, height, title)
     InitWindow( 2*offset + cellSize * cellCount, 2*offset + cellSize * cellCount, "Retro Snake" );
   
-    SetTargetFPS(60);
-
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+    
     Game game;
     
-    // WindowShouldClose returns true if esc is pressed or exit button
+    // returns true if esc is pressed or exit button
     while (WindowShouldClose() == false) {
+        
         UpdateMusicStream(game.backgroundSound);
+        
         //Prepare to render a new frame
         BeginDrawing();
         
@@ -173,6 +128,7 @@ int main () {
         if (IsKeyPressed(KEY_UP) && game.snake.direction.y != 1) {
             game.snake.direction = {0, -1};
             game.running = true;
+            ResumeMusicStream(game.backgroundSound);
         }
         if (IsKeyPressed(KEY_DOWN) && game.snake.direction.y != -1) {
             game.snake.direction = {0, 1};
@@ -188,52 +144,46 @@ int main () {
 
         }
         
-        ClearBackground(green);
+        ClearBackground(green);// clear the background to green color
+        // draw game border 
         DrawRectangleLinesEx(Rectangle {(float)offset-5, (float)offset-5, (float)cellSize * cellCount + 10, (float)cellSize * cellCount + 10}, 5,  darkGreen );
+        // draw game title text
         DrawText("Retro Snake", offset -5, 20, 40, darkGreen);
+        // draw score text
         DrawText(TextFormat("%i", game.score), offset - 5, offset + cellSize * cellCount + 10, 40, darkGreen);
+        // draw game objects (Snake and Apple)
         game.Draw();
 
-        EndDrawing(); // ends the current frames drawing phase
+        // ends the current frames drawing phase
+        EndDrawing();
+
+        UpdateMusicStream(game.backgroundSound);
         
     }
-        
+
+    
     CloseWindow();
     
     return 0;
-    // const int SCREEN_WIDTH = 800;
-    // const int SCREEN_HEIGHT = 600;
-    // int ball_x = 100;
-    // int ball_y = 100;
-    // int ball_speed_x = 5;
-    // int ball_speed_y = 5;
-    // int ball_radius = 15;
-    //
-    // cout << "Hello World" << endl;
-    //
-    // InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My first RAYLIB program!");
-    // SetTargetFPS(60);
-    //
-    // while (WindowShouldClose() == false){
-    //
-    //     ball_x += ball_speed_x;
-    //     ball_y += ball_speed_y;
-    //
-    //     if(ball_x + ball_radius >= SCREEN_WIDTH || ball_x - ball_radius <= 0)
-    //     {
-    //         ball_speed_x *= -1;
-    //     }
-    //
-    //     if(ball_y + ball_radius >= SCREEN_HEIGHT || ball_y - ball_radius <= 0)
-    //     {
-    //         ball_speed_y *= -1;
-    //     }
-    //     
-    //     BeginDrawing();
-    //     ClearBackground(BLACK);
-    //     DrawCircle(ball_x,ball_y,ball_radius, WHITE);
-    //     EndDrawing();
-    // }
-    //
-    // CloseWindow();
 }
+
+bool elementInDeque(Vector2 element, deque<Vector2> deque) {
+    for (unsigned int i = 0; i < deque.size(); i++) {
+        // if head position matches any apple position, return true
+        if (deque[i] == element) {
+            return true;
+        }
+    }
+    return false;
+}
+bool eventTriggered(double interval) {
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= interval) {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
