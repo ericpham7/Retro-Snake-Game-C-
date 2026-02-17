8a7410aa1f4990c3ee5a5c5994f6110f94399256
EricSyntax <ericpham0902@gmail.com>
2026-02-17 13:01:53 -0800

    Fix web build: update C++ for Emscripten, increase stack size, add shell.html

--- Food.cpp
+++ Food.cpp
@@ -1,1 +1,47 @@
-#include "raylib.h"#include "Food.h"#include <iostream>#include <deque>#include "globalVariables.h"using namespace std;    // Food Object Constructor     Food::Food(deque<Vector2> snakeBody) {        Image image = LoadImage("greenapple.png");        ImageResize(&image, cellSize, cellSize);  // Resize food image to 30x30 pixels        texture = LoadTextureFromImage(image);        UnloadImage(image); // unload image to free memory              position = GenerateRandomPosition(snakeBody); // set initial food position    }    // Texture destructor    Food::~Food() {        UnloadTexture(texture); // unload texture to free memory after food object is destroyed    }    // Draw Food on Screen Method    void Food::Draw() {         DrawTexture(texture, offset + position.x * cellSize, offset + position.y * cellSize, WHITE);         position = GenerateRandomCell();     }        // Position Randomizer    Vector2 Food::GenerateRandomCell() {        float x = GetRandomValue(0, cellCount -1);        float y = GetRandomValue(0, cellCount -1);        return Vector2{x, y};    }        // Position Randomizer    Vector2 Food::GenerateRandomPosition(deque<Vector2> snakeBody) {        Vector2 position = GenerateRandomCell();         // while food object position spawns in same position as snake body, generate a new position        while (elementInDeque(position, snakeBody)) {            position = GenerateRandomCell();        }                 return position;    } 
+#include "Food.h"
+#include "globalVariables.h"
+#include "raylib.h"
+#include <deque>
+#include <iostream>
+
+using namespace std;
+
+// Food Object Constructor
+Food::Food(const deque<Vector2> &snakeBody) {
+  Image image = LoadImage("greenapple.png");
+  ImageResize(&image, cellSize, cellSize); // Resize food image to 30x30 pixels
+  texture = LoadTextureFromImage(image);
+  UnloadImage(image);              // unload image to free memory
+  position = GenerateRandomCell(); // set initial food position
+}
+
+// Texture destructor
+Food::~Food() {
+  UnloadTexture(
+      texture); // unload texture to free memory after food object is destroyed
+}
+
+// Draw Food on Screen Method
+void Food::Draw() {
+  DrawTexture(texture, offset + position.x * cellSize,
+              offset + position.y * cellSize, WHITE);
+}
+
+// Position Randomizer
+Vector2 Food::GenerateRandomCell() {
+  float x = GetRandomValue(0, cellCount - 1);
+  float y = GetRandomValue(0, cellCount - 1);
+  return (Vector2){x, y};
+}
+
+// Position Randomizer (ensures food doesn't spawn inside snake body)
+Vector2 Food::GenerateRandomPosition(const deque<Vector2> &snakeBody) {
+  Vector2 position = GenerateRandomCell();
+
+  // while food position overlaps with snake body, generate a new position
+  while (elementInDeque(position, snakeBody)) {
+    position = GenerateRandomCell();
+  }
+
+  return position;
+}
